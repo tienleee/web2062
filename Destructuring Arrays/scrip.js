ffbbb578a586dce6ccfd07f8df91cbb3f12917f4
tienlee <vuletien2003@gmail.com>
2023-01-09 15:51:06 +0700

    tienlee

--- Destructuring Arrays/scrip.js
+++ Destructuring Arrays/scrip.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const restaurant = {
-    name: 'Classico Italiano',
+    name: 'tienlee',
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    starterMenu: ['caran', 'cachien', 'canau', 'ca'],
+    mainMenu: ['caran', 'cachien', 'canau'],
 
     order: function(starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -24,25 +24,20 @@ console.log(arr);
 let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
 
-// Switching variables
-// const temp = main;
-// main = secondary;
-// secondary = temp;
-// console.log(main, secondary);
+
 
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
 
-// Receive 2 return values from a function
+
 const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
 
-// Nested destructuring
 const nested = [2, 4, [5, 6]];
-// const [i, , j] = nested;
+
 const [i, , [j, k]] = nested;
 console.log(i, j, k);
 
-// Default values
+
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
